1e7cf5fa43ae379bc39734f21559e8fa84f1c060
Randall John <randall.john36@gmail.com>
2022-11-07 21:53:24 -0500

    fixed some sql connection syntax

--- session-management/src/main/java/com/sessionmanagement/registration/RegistrationServlet.java
+++ session-management/src/main/java/com/sessionmanagement/registration/RegistrationServlet.java
@@ -1,5 +1,6 @@
 package com.sessionmanagement.registration;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Servlet implementation class RegistrationServlet
@@ -27,15 +29,39 @@ public class RegistrationServlet extends HttpServlet {
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
 		
+		RequestDispatcher dispatcher = null;
+		Connection connection = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql:localhost:3306/session_management", "root", "Exexxexex2*");
-			PreparedStatement pst = con.prepareStatement("insert into users(name,password,email,phone) values(?,?,?,?)");
-		} catch (Exception e) {
+			connection = DriverManager.getConnection("jdbc:mysql:localhost:3306/session_management", "root", "Exexxexex2*");
+			PreparedStatement pst = connection.prepareStatement("insert into users(name,password,email,phone) values(?,?,?,?)");
 			
+			pst.setString(1, name);
+			pst.setString(2, password);
+			pst.setString(3, email);
+			pst.setString(4, phone);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount > 0) {
+				request.setAttribute("status", "success");
+				
+			} else {
+				request.setAttribute("status", "success");
+			}
+			
+			dispatcher.forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
-
 }
